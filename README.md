8467232cf49f06a397852ddee1521a5987f09bb2
jcha0713 <joocha0713@gmail.com>
2022-08-08 17:14:57 -0700

    feat: Use treesitter to choose when to load the source

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # cmp-tw2css
 
 A source for [`nvim-cmp`](https://github.com/hrsh7th/nvim-cmp) to convert [tailwindcss](https://tailwindcss.com) classes to pure css codes.
+It uses treesitter to find out whether any css code block exists in the code and load the completion source.
 
 ## Setup
 
@@ -36,11 +37,13 @@ require('cmp').setup {
 
 ## Usage
 
-Using `cmp-tw2css` is simple. Open any file with `.css`, `.scss`, or `.less` extension and start typing tailwindcss class that you want to convert to css codes.
+Using `cmp-tw2css` is simple. Open any file that contains css code blocks and start typing tailwindcss classes that you want to convert into css codes.
 
 ### Example
 
 ![demo](./extra/demo.gif)
+
+- `*.css`:
 
 ```css
 body {
@@ -50,21 +53,38 @@ body {
 }
 ```
 
+- `*.html`:
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <style>
+      body {
+        /* m-2 -> margin: 0.5rem; */
+      }
+    </style>
+  </head>
+  <body>
+    <!-- body -->
+  </body>
+</html>
+```
+
 ## Limitation
 
 There are a number of limitations to `cmp-tw2css`. First, the source of this plugin is a result of web scraping. This means that you might find some items are missing while using. If this happens to you, please let me know by submitting an issue so that I can update the source accordingly. Another downside is that it can't be dynamically generated and only provides the code from the official website. 
 
-Currently `cmp-tw2css` cannot detect whether the cursor is inside the code block or not. And when the insert text is more than one line, it does not automatically add tabs for the additional lines.
+Currently `cmp-tw2css` does not automatically add tabs for the additional lines when the insert text is more than one line. And also when there are more than one colon(`:`), `cmp-tw2css` cannot properly load its completion source.
 
 ## Roadmap
 
-- Load the source only when the cursor is inside the code block
-- Let the users to decide which type of file they want to use (css / scss / less)
+- [x] Load the source only when the cursor is inside the code block
+- [ ] Provide ways to configure with treesitter
+- [ ] Show documentation when selecting an item...?
 
 ## Credit
 
 - [`cmp-emoji`](https://github.com/hrsh7th/cmp-emoji)
 - [`cmp-npm`](https://github.com/David-Kunz/cmp-npm)
-
-----------
-Last updated date of tailwindcss class lists: 8/6/2022
+- [`nvim-treesitter`](https://github.com/nvim-treesitter/nvim-treesitter)
--- lua/cmp-tw2css/init.lua
+++ lua/cmp-tw2css/init.lua
@@ -1,37 +1,183 @@
 local source = {}
 
--- Constructor function
+---@class cmp-tw2css.source
+---@field public is_sorted boolean
+---@field public items table<string, string>
 source.new = function()
   local self = setmetatable({}, { __index = source })
+  self.is_sorted = false
+  self.items = {}
   return self
 end
 
--- Return whether this source is available in the current context or not (optional).
--- @return boolean
-function source:is_available()
-  -- it only works for files end with *.css, *.scss, and *.less
-  local filename = vim.fn.expand("%:e")
-  return filename == "css" or filename == "scss" or filename == "less"
+--- Checks if the lang is css or scss
+---@param lang string
+---@return boolean
+local function is_stylesheet(lang)
+  return lang == "css" or lang == "scss"
 end
 
--- Return the debug name of this source (optional).
--- @return string
+--- Get the language set to the buffer
+---@param bufnr number
+local function get_buf_lang(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  return vim.api.nvim_buf_get_option(bufnr, "ft")
+end
+
+---@return boolean
+local function iter_tree(tree)
+  local lang = tree:lang()
+
+  if is_stylesheet(lang) then
+    return true
+  end
+
+  if tree:children() ~= nil then
+    for child_lang, child_tree in pairs(tree:children()) do
+      print("child lang: " .. child_lang)
+      if is_stylesheet(child_lang) then
+        return true
+      else
+        return iter_tree(child_tree)
+      end
+    end
+  end
+
+  return true
+end
+
+--- Return whether this source is available in the current context or not.
+---@return boolean
+function source:is_available()
+  local bufnr = vim.api.nvim_get_current_buf() or 0
+  local buf_lang = get_buf_lang(bufnr)
+  local ok, tree = pcall(vim.treesitter.get_parser, bufnr, buf_lang)
+
+  -- if there is no treesitter parser then look at the file extension
+  if not ok then
+    local filename = vim.fn.expand("%:e")
+    if not filename then
+      return false
+    end
+    return is_stylesheet(filename)
+  end
+
+  return iter_tree(tree)
+end
+
+--- Return the debug name of this source.
+---@return string
 function source:get_debug_name()
   return "cmp-tw2css"
 end
 
--- Invoke completion (required).
--- @param params cmp.SourceCompletionApiParamsi
--- @param callback fun(response: lsp.CompletionResponse|nil)
+--- Copied from nvim-treesitter.ts_utils
+--- Determines whether (line, col) position is in node range
+-- @param node Node defining the range
+-- @param line A line (0-based)
+-- @param col A column (0-based)
+local function is_in_node_range(node, line, col)
+  local start_line, start_col, end_line, end_col = node:range()
+  if line >= start_line and line <= end_line then
+    if line == start_line and line == end_line then
+      return col >= start_col and col < end_col
+    elseif line == start_line then
+      return col >= start_col
+    elseif line == end_line then
+      return col < end_col
+    else
+      return true
+    end
+  else
+    return false
+  end
+end
+
+--- Copied from nvim-treesitter.ts_utils
+--- Get the root node of the tree
+local function get_root_for_position(line, col, root_lang_tree)
+  local lang_tree = root_lang_tree:language_for_range({ line, col, line, col })
+
+  for _, tree in ipairs(lang_tree:trees()) do
+    local root = tree:root()
+
+    if root and is_in_node_range(root, line, col) then
+      return root, tree, lang_tree
+    end
+  end
+
+  return nil, nil, lang_tree
+end
+
+--- Get the items table and sort the items
+--- If the table is already sorted, then simply return it
+---@return table<string, string> items
+function source:get_sorted_items()
+  if not self.is_sorted then
+    -- if items table is not sorted, then sort it
+    self.items = require("cmp-tw2css.items")()
+    table.sort(self.items, function(a, b)
+      return a.label < b.label
+    end)
+
+    -- the table is now sorted
+    self.is_sorted = true
+  end
+
+  return self.items
+end
+
+--- Invoke completion (required).
+---@param params cmp.SourceCompletionApiParams
+---@param callback fun(response: lsp.CompletionResponse|nil)
 function source:complete(params, callback)
-  -- custom sorting
-  local items = require("cmp-tw2css.items")()
+  local bufnr = vim.api.nvim_get_current_buf() or 0
+  local buf_lang = get_buf_lang(bufnr)
+  local ok, root_lang_tree = pcall(vim.treesitter.get_parser, bufnr, buf_lang)
 
-  table.sort(items, function(a, b)
-    return a.label < b.label
-  end)
+  -- no treesitter, no completion
+  if not ok then
+    callback()
+    return
+  end
 
-  callback(items)
+  local cursor = params.context.cursor
+  local cur_line = params.context.cursor_line
+
+  local root = get_root_for_position(cursor.row, cursor.col, root_lang_tree)
+
+  if not root then
+    callback()
+    return
+  end
+
+  local node_at_cursor = root:named_descendant_for_range(
+    cursor.row - 1,
+    cursor.col - 1,
+    cursor.row - 1,
+    cursor.col - 1
+  )
+
+  -- prevent loading items when the cursor is after the colon
+  --- TODO: Allow users to write single line css
+  local in_property_name = true
+  local idx_colon = string.find(cur_line, ":%s*(%w+)")
+  if idx_colon then
+    if cursor.col > idx_colon then
+      in_property_name = false
+    end
+  end
+
+  local node_at_cursor_type = node_at_cursor:type()
+
+  if node_at_cursor_type == "block" or node_at_cursor_type == "declaration" then
+    if in_property_name then
+      local items = source:get_sorted_items()
+      callback(items)
+      return
+    end
+  end
+  callback()
 end
 
 return source
